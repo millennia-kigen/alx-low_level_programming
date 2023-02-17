ab50e3131cc38c57ede55ca1307841eff98e9576
millennia-kigen <millenniakigen@gmail.com>
2023-02-17 13:31:17 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+{
+putchar(lc);
+}
+putchar('\n');
+return (0);
+}
