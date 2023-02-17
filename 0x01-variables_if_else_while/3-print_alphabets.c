db6b5ccb893e2c3b0d00fd8c77fb75ff1f4b4ef6
millennia-kigen <millenniakigen@gmail.com>
2023-02-17 13:45:51 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+for(ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-for (ch = 'A'; ch <= 'z'; ch++)
+for(ch = 'A'; ch <= 'z'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
