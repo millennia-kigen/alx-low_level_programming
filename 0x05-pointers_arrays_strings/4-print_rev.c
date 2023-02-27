f94cb3d0354802636abec19d2bded7442267db56
millennia-kigen <millenniakigen@gmail.com>
2023-02-27 05:30:26 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,14 @@
 void print_rev(char *s)
 {
 int longi = 0;
-int 0;
+int o;
 while (*s != '\0')
 {
 longi++;
 s++;
 }
 s--;
-for (0 = longi; 0 > 0; 0--)
+for (o = longi; o > 0; o--)
 {
 _putchar(*s);
 s--;
