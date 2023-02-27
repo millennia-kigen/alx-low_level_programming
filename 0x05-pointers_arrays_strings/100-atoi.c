174d2fa5fc9940d966a6c26e28e8c5a434f072b8
millennia-kigen <millenniakigen@gmail.com>
2023-02-27 08:59:19 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,11 @@ if (s[i] == '-')
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
-if (s[i + 1] < '0' \\ s[i + 1] > '9')
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
 f = 0;
 }
