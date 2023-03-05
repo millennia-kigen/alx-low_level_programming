3e5c22de7513f592f1c0d298425130d02c220bf1
millennia-kigen <millenniakigen@gmail.com>
2023-03-05 14:38:34 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: 0 (success)
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c;
-char d;
-c = 'a';
-d = 'A';
-while
-(c <= 'z')
-{putchar(c);
-c++;
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
-while
-(d <= 'z')
-{putchar(c);
-d++;
-}
-putchar('\n');
-return (0);
-}
+
